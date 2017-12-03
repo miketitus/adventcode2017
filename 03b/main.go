d184b8f51f8fa99bd068e9dc0164b41234fa0633
miketitus <mike@mike-titus.com>
2017-12-03 13:37:57 -0800

    stash work

--- 03b/main.go
+++ 03b/main.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-const INPUT = 47
+const INPUT = 24
 const (
 	INIT  = iota
 	RIGHT = iota
@@ -19,9 +19,9 @@ func main() {
 	root := int(math.Ceil(math.Sqrt(float64(INPUT))))
 	fmt.Println("root = ", root)
 	// initialize array
-	arry := make([][]int, root)
-	for i := range arry {
-		arry[i] = make([]int, root)
+	memory := make([][]int, root)
+	for i := range memory {
+		memory[i] = make([]int, root)
 	}
 	direction := INIT
 	x := int(root / 2)
@@ -33,28 +33,48 @@ func main() {
 			direction = RIGHT
 		case RIGHT:
 			y++
-			if arry[x-1][y] == 0 {
+			if memory[x-1][y] == 0 {
 				direction = UP
 			}
 		case LEFT:
 			y--
-			if arry[x+1][y] == 0 {
+			if memory[x+1][y] == 0 {
 				direction = DOWN
 			}
 		case UP:
 			x--
-			if arry[x][y-1] == 0 {
+			if memory[x][y-1] == 0 {
 				direction = LEFT
 			}
 		case DOWN:
 			x++
-			if arry[x][y+1] == 0 {
+			if memory[x][y+1] == 0 {
 				direction = RIGHT
 			}
 		}
-		arry[x][y] = j
+		memory[x][y] = adjacentSum(memory, x, y, root)
 	}
-	print2D(arry)
+	print2D(memory)
+}
+
+func adjacentSum(a [][]int, x int, y int, root int) int {
+	xSet := []int{x}
+	ySet := []int{y}
+	if 0 != x {
+		xSet = append(xSet, x-1)
+	}
+	if root != x {
+		xSet = append(xSet, x+1)
+	}
+	if 0 != y {
+		ySet = append(ySet, y-1)
+	}
+	if root != y {
+		ySet = append(ySet, y+1)
+	}
+	fmt.Println("xSet = ", xSet)
+	fmt.Println("ySet = ", ySet)
+	return 1
 }
 
 func print2D(a [][]int) {
