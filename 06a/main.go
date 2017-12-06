2f461195a887b510aa8255153dab9f5573e40f8f
miketitus <mike@mike-titus.com>
2017-12-06 13:02:20 -0800

    stash before debugging

--- 06a/main.go
+++ 06a/main.go
@@ -1,29 +1,50 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 type bank int
 
-var banks = []bank{0, 2, 7, 0}
-var banks1 = []bank{11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11}
+var banks1 = []bank{0, 2, 7, 0}
+var banks = []bank{11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11}
 
 func main() {
     fmt.Println(banks)
     cycles := 0
-    states := [][]bank{}
+    states := []string{}
+    states = append(states, fmt.Sprint(banks))
+    Outer:
     for {
-        // store current config
-        states = append(states, banks)
         // generate new config
-        max := mostBlocks(banks)
-        fmt.Println("max = ", max)
-        // test for previous state
+        maxBlock := mostBlocks(banks)        
+        rebalance(banks, maxBlock)
         cycles++
-        if cycles > 0 {
-            break
+        // test for previous state
+        //fmt.Println("states ", states)
+        for _, s := range states {
+            //fmt.Println("testing ", banks, s)
+            if s == fmt.Sprint(banks) {
+                break Outer
+            }
         }
+        // store current state
+        states = append(states, fmt.Sprint(banks))
     }
     fmt.Println(cycles)
+    fmt.Println(banks)
+}
+
+func rebalance(b []bank, maxBlock int) {
+    blocks := b[maxBlock]
+    b[maxBlock] = 0
+    for i := maxBlock+1 ; blocks > 0; i++ {
+        if i == len(b) {
+            i = 0
+        }
+        b[i]++
+        blocks--
+    }
 }
 
 func mostBlocks(b []bank) int {
