cff3ffa19385c7e3a926117252fb2af40d62f533
miketitus <mike@mike-titus.com>
2017-12-11 18:57:36 -0800

    save basic circular list

--- 10a/main.go
+++ 10a/main.go
@@ -1,56 +1,87 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"sort"
+	"strconv"
 )
 
 const size = 5
 
-var list []int
+var list circularList
 var input = []int{3, 4}
 var input1 = []int{
 	83, 0, 193, 1, 254, 237, 187, 40, 88, 27, 2, 255, 149, 29, 42, 100,
 }
 
+type cNode struct {
+	value int
+	next  *cNode
+	prev  *cNode
+}
+
+type circularList struct {
+	zeroNode *cNode
+}
+
+func (cl *circularList) addNode(cn *cNode) {
+	if cl.zeroNode == nil {
+		cl.zeroNode = cn
+		cn.next = cn
+		cn.prev = cn
+	} else {
+		// append to "end" of list (before zeroNode)
+		oldEnd := cl.zeroNode.prev
+		oldEnd.next = cn
+		cn.next = cl.zeroNode
+		cn.prev = oldEnd
+		cl.zeroNode.prev = cn
+	}
+}
+
+func (cl circularList) String() string {
+	buf := bytes.NewBufferString("[")
+	if cl.zeroNode == nil {
+		// write nothing
+	} else {
+		buf.WriteRune(' ')
+		for start := cl.zeroNode; ; start = start.next {
+			buf.WriteString(strconv.Itoa(start.value))
+			buf.WriteRune(' ')
+			if start.next == cl.zeroNode {
+				break
+			}
+		}
+	}
+	buf.WriteString("]")
+	return buf.String()
+}
+
 func main() {
 	// initialize list
-	list = make([]int, size, size)
+	list = circularList{}
 	for i := 0; i < size; i++ {
-		list[i] = i
+		node := cNode{value: i}
+		list.addNode(&node)
 	}
+	fmt.Println(list)
 	// process input
-	position := 0
-	skipSize := 0
-	for _, length := range input {
-		fmt.Printf("---- length %d ----\n", length)
-		var sublist []int
-		if position+length > len(list) {
-			// need to wrap
-			end1 := list[position:]
-			beg1 := list[:len(list)-position]
-			sublist = append(end1, beg1...)
-			fmt.Printf("end1:%v, beg1:%v, sub:%v\n", end1, beg1, sublist)
-			// sort.Reverse gives wrong results, so reverse manually :(
-			k := len(sublist) - 1
-			for j := 0; j < k; j++ {
-				z := sublist[j]
-				sublist[j] = sublist[k]
-				sublist[k] = z
-				k--
-			}
-			end2 := sublist[position:]
-			beg2 := sublist[:len(sublist)-position]
-			fmt.Printf("end2:%v, beg2:%v, sub:%v\n", end2, beg2, sublist)
-		} else {
-			sublist = list[position:length]
-			sort.Sort(sort.Reverse(sort.IntSlice(sublist)))
-			remainder := list[(position + length):]
-			list = append(sublist, remainder...)
+	/*
+		position := 0
+		skipSize := 0
+		for _, length := range input {
+			reverseSection(position, length)
+			position += length + skipSize
+			skipSize++
+			fmt.Printf("pos:%d, ss:%d\n", position, skipSize)
+			fmt.Printf("list:%v\n", list)
 		}
-		position += length + skipSize
-		skipSize++
-		fmt.Printf("pos:%d, ss:%d, sublist:%v\n", position, skipSize, sublist)
-		fmt.Printf("list:%v\n", list)
+	*/
+}
+
+func reverseSection(p, l int) {
+	if l <= 1 {
+		// nothing to reverse
+		return
 	}
 }
