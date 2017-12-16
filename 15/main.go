8e85daae8e099f76f7b03202f9bd86e7cd6dd9fc
miketitus <mike@mike-titus.com>
2017-12-16 11:51:02 -0800

    complete part one

--- 15/main.go
+++ 15/main.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-var inputA uint64 = 65
+var inputA uint64 = 634
 var factorA uint64 = 16807
-var inputB uint64 = 8921
+var inputB uint64 = 301
 var factorB uint64 = 48271
 var divisor uint64 = 2147483647
 var mask uint64 = 0x0000FFFF
@@ -16,11 +16,8 @@ func main() {
 		inputB = (inputB * factorB) % divisor
 		bitsA := inputA & mask
 		bitsB := inputB & mask
-		//fmt.Printf("inputA = %x, input B = %x\n", inputA, inputB)
-		//fmt.Printf("bitsA = %x, bitsB = %x\n", bitsA, bitsB)
 		if bitsA == bitsB {
 			matches++
-			fmt.Printf("match at %d  %16b  %16b\n", i, bitsA, bitsB)
 		}
 	}
 	fmt.Printf("total matches = %d\n", matches)
