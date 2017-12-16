91a4320900ff02126125e72d2df0d0420da45f97
miketitus <mike@mike-titus.com>
2017-12-16 14:13:20 -0800

    final version

--- 15b/main.go
+++ 15b/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-var pairs = 16
-var inputA uint64 = 65
+var pairs = 5000000
+var inputA uint64 = 634
 var factorA uint64 = 16807
-var inputB uint64 = 8921
+var inputB uint64 = 301
 var factorB uint64 = 48271
 var divisor uint64 = 2147483647
 var mask uint64 = 0x0000FFFF
@@ -16,29 +16,27 @@ var mask uint64 = 0x0000FFFF
 func main() {
 	chA := make(chan uint64)
 	chB := make(chan uint64)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
 	go func() {
 		// generator A
-		for i := 0; i < pairs; i++ {
+		for true {
 			inputA = (inputA * factorA) % divisor
 			if inputA%4 == 0 {
-				fmt.Println("iA =", i)
 				chA <- inputA & mask
 			}
 		}
-		close(chA)
 	}()
 
 	go func() {
 		// generator B
-		for i := 0; i < pairs; i++ {
+		for true {
 			inputB = (inputB * factorB) % divisor
 			if inputB%8 == 0 {
-				fmt.Println("iB =", i)
 				chB <- inputB & mask
 			}
 		}
-		close(chB)
 	}()
 
 	go func() {
@@ -47,13 +45,13 @@ func main() {
 		for i := 0; i < pairs; i++ {
 			bitsA := <-chA
 			bitsB := <-chB
-			fmt.Printf("bitsA = %d, bitsB = %d\n", bitsA, bitsB)
 			if bitsA == bitsB {
 				matches++
 			}
 		}
 		fmt.Printf("total matches = %d\n", matches)
+		wg.Done()
 	}()
 
-	time.Sleep(8 * time.Second)
+	wg.Wait()
 }
